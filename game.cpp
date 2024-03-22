cdea8ba3d291e6cd4e0136433025b397417a1d80
YojimboMusashi <59484387+YojimboMusashi@users.noreply.github.com>
2024-03-22 16:45:50 +0000

    name changes
    ship is now player, player is now bouncingball

--- game.cpp
+++ game.cpp
@@ -1,9 +1,9 @@
-#include "player.h"
+#include "bouncyball.h"
 #include "Enemy.h"
-#include "ship.h"
+#include "player.h"
 
-Player mercury;
-Ship bob;
+BouncyBall p1;
+Player bob;
 
 void GameSetup() {
 }
@@ -13,6 +13,6 @@ void GameRun() {
 	//draw_triangle(400, 400, 400, 600, 600, 600, 0x00ff00);
 	//draw_rect_xywh(600, 500, 100, 100, RGB(255,0,255));	
 	bob.draw();
-	//mercury.draw();
+	p1.draw();
 	draw_grid(10, 5, 0x00ff00);
 }
--- player.h
+++ player.h
@@ -2,29 +2,90 @@ class Player {
 public:
 	int x = 500;
 	int y = 500;
-	int r = 20;
-	float xvel = 50.0f;
-	float yvel = 50.0f;
-	u32 color = 0x550000;
+	int w = 50;
+	int h = 50;
+	float speed = 10.0f;
+	float xvel = 0;
+	float yvel = 0;
+	bool m_up = false;
+	bool m_left = false;
+	bool m_down = false;
+	bool m_right = false;
+	bool le = false;
+	u32 color = 0x00ff00;
 
 public:
 	void move();
+	void up(bool state);
+	void left(bool state);
+	void down(bool state);
+	void right(bool state);
 	void draw();
 };
 
+void Player::up(bool state) {
+	m_up = state;
+}
+void Player::left(bool state) {
+	m_left = state;
+}
+void Player::down(bool state) {
+	m_down = state;
+}
+void Player::right(bool state) {
+	m_right = state;
+}
 
-void Player::move() {	
-	if (x + r + xvel >= render_state.width || x - r + xvel < 0) {
-		xvel = xvel * -1;
+void Player::move() {
+	if (m_right == true) {
+		xvel = speed;
 	}
-	if (y + r + yvel >= render_state.height || y - r + yvel < 0) {
-		yvel = yvel * -1;
+	else if (m_left == true) {
+		xvel = -speed;
 	}
+	else {
+		xvel = 0.0f;
+	}
+
+	if (m_up == true) {
+		yvel = speed;
+	}
+	else if (m_down == true) {
+		yvel = -speed;
+	}
+	else {
+		yvel = 0.0f;
+	}
+
+
+	if (x + w + xvel >= render_state.width || x + xvel < 0) {
+		if (x + w + xvel >= render_state.width) {
+			x = render_state.width - w;
+		}
+		if (x + xvel < 0) {
+			x = 0;
+		}
+		xvel = 0.0f;
+	}
+
+	if (y + h + yvel >= render_state.height || y + yvel < 0) {
+		if (y + h + yvel >= render_state.height) {
+			y = render_state.height - h;
+		}
+		if (y + yvel < 0) {
+			y = 0;
+		}
+		yvel = 0.0f;
+	}
+
 	x += xvel;
 	y += yvel;
 }
 
 void Player::draw() {
 	move();
-	draw_circle(x, y, r, color);
+	draw_rect_xywh(x, y, w, h, color);
 }
+
+
+
